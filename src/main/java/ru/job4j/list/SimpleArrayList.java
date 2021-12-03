4e18d2030e470f2dc1eb5b9859c2d9483a4b079a
Zver <krutaxe@mail.ru>
2021-12-03 18:12:50 +0300

    1. Динамический список на массиве. [#158 #235145]

--- src/main/java/ru/job4j/list/SimpleArrayList.java
+++ src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -14,20 +14,26 @@ public class SimpleArrayList<T> implements List<T> {
         this.container = (T[]) new Object[capacity];
     }
 
-    @Override
-    public void add(T value) {
+    public void capacityIncrease() {
         if (size >= container.length) {
             container = Arrays.copyOf(container,
-            container.length * 2);
+                    container.length * 2);
+        } else if (container.length == 0) {
+            container = Arrays.copyOf(container, 10);
         }
+    }
+
+    @Override
+    public void add(T value) {
+        capacityIncrease();
         container[size++] = value;
         modCount++;
     }
 
     @Override
     public T set(int index, T newValue) {
-        Objects.checkIndex(index, container.length);
-        T temp = container[index];
+        Objects.checkIndex(index, size);
+        T temp = get(index);
         container[index] = newValue;
         modCount++;
         return temp;
@@ -36,14 +42,14 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T remove(int index) {
-        Objects.checkIndex(index, container.length);
-        T temp = container[index];
+        Objects.checkIndex(index, size);
+        T temp = get(index);
         System.arraycopy(container,
                 index + 1,
                 container,
                 index,
-                container.length - index - 1);
-        container[container.length - 1] = null;
+                size - index - 1);
+        container[size - 1] = null;
         modCount++;
         size--;
         return temp;
@@ -51,7 +57,7 @@ public class SimpleArrayList<T> implements List<T> {
 
     @Override
     public T get(int index) {
-        Objects.checkIndex(index, container.length);
+        Objects.checkIndex(index, size);
         return container[index];
     }
 
