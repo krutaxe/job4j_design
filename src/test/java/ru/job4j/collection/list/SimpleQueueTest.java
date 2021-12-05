e6b8a24d815dcd5e15748d320b9d3c084581e92f
Zver <krutaxe@mail.ru>
2021-12-05 13:58:41 +0300

    5. Очередь на двух стеках [#160]

---
+++ src/test/java/ru/job4j/collection/list/SimpleQueueTest.java
@@ -0,0 +1,66 @@
+package ru.job4j.collection.list;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+import org.junit.Test;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueueTest {
+
+    @Test
+    public void whenPushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        int rsl = queue.poll();
+        assertThat(rsl, is(1));
+    }
+
+    @Test
+    public void when2PushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        int rsl = queue.poll();
+        assertThat(rsl, is(1));
+    }
+
+    @Test
+    public void when2PushPollPushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.poll();
+        queue.push(2);
+        int rsl = queue.poll();
+        assertThat(rsl, is(2));
+    }
+
+    @Test(expected = NoSuchElementException.class)
+    public void whenEmptyPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.poll();
+    }
+    @Test
+    public void whenPushPushPollAndPush() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        queue.poll();
+        queue.push(3);
+        assertThat(queue.poll(), is(2));
+    }
+
+    @Test
+    public void whenPushPushPushPushPollPollAndPushPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        queue.push(1);
+        queue.push(2);
+        queue.push(3);
+        queue.push(4);
+        assertThat(queue.poll(), is(1));
+        assertThat(queue.poll(), is(2));
+        queue.push(5);
+        assertThat(queue.poll(), is(3));
+    }
+}
