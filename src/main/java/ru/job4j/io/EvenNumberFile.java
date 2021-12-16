9537917919b93dece4010f31b02b0075bfa47dab
Zver <krutaxe@mail.ru>
2021-12-16 22:00:30 +0300

    0.2. FileInputStream [#4898]

---
+++ src/main/java/ru/job4j/io/EvenNumberFile.java
@@ -0,0 +1,19 @@
+package ru.job4j.io;
+
+import java.io.*;
+
+public class EvenNumberFile {
+    public static void main(String[] args) {
+        try (BufferedReader in = new BufferedReader(new FileReader("even.txt"))) {
+            String line;
+            while ((line = in.readLine()) != null) {
+                int i = Integer.parseInt(line);
+                if (i % 2 == 0) {
+                    System.out.println(i);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
