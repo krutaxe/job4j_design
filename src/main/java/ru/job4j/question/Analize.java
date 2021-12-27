64cc9d542c1bbcc768012d8d445885d1a7c8a0cd
Zver <krutaxe@mail.ru>
2021-12-27 23:18:01 +0300

    2. Статистика по коллекции. [#45889]

--- src/main/java/ru/job4j/question/Analize.java
+++ src/main/java/ru/job4j/question/Analize.java
@@ -2,34 +2,30 @@ package ru.job4j.question;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
 public class Analize {
 
     public static Info diff(Set<User> previous, Set<User> current) {
-        int add = 0;
         int change = 0;
         int del = 0;
-        Info info = new Info(add, change, del);
+
         Map<Integer, String> currentMap = current.stream()
                 .collect(Collectors.toMap(User::getId, User::getName));
         for (User user : previous) {
             String currentName = currentMap.get(user.getId());
-            if (currentName != null && !currentMap.get(user.getId()).equals(user.getName())) {
+            if (currentName != null && !Objects.equals(currentName, user.getName())) {
                 change++;
                 currentMap.remove(user.getId());
-            } else if (currentName != null && currentMap.get(user.getId()).equals(user.getName())) {
+            } else if (currentName != null && Objects.equals(currentName, user.getName())) {
                 currentMap.remove(user.getId());
             } else if (currentName == null) {
                 del++;
             }
         }
-        add += currentMap.size();
-        info.setAdded(add);
-        info.setChanged(change);
-        info.setDeleted(del);
-    return info;
+    return new Info(currentMap.size(), change, del);
     }
 
 }
