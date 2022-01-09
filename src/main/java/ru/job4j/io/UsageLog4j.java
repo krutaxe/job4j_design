46791d07014591c93f23e20424220d6062f2cc5b
Zver <krutaxe@mail.ru>
2022-01-09 19:43:13 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -8,10 +8,32 @@ public class UsageLog4j {
     private static final Logger LOG = LoggerFactory.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
-        LOG.trace("trace message");
-        LOG.debug("debug message");
-        LOG.info("info message");
-        LOG.warn("warn message");
-        LOG.error("error message");
+        String nameStudent = "Petr Arsentev";
+        int age = 33;
+        byte course = 3;
+        char gender = 'M';
+        double grade = 4.5;
+        float height = 1.80f;
+        long scholarship = 500000000;
+        boolean militaryDepartment = true;
+        float weight = 80;
+
+        LOG.debug("Student info: name-{}, age-{}, course-{}, gender-{}, "
+                + "grade-{}, height-{}, scholarship-{}, militaryDepartment-{}, weight-{}",
+                  nameStudent, age, course, gender, grade, height,
+                scholarship, militaryDepartment, weight);
+        LOG.error("Student info: name-{}, age-{}, course-{}, gender-{}, "
+                        + "grade-{}, height-{}, scholarship-{}, militaryDepartment-{}, weight-{}",
+                nameStudent, age, course, gender, grade, height,
+                scholarship, militaryDepartment, weight);
+        LOG.info("Student info: name-{}, age-{}, course-{}, gender-{}, "
+                        + "grade-{}, height-{}, scholarship-{}, militaryDepartment-{}, weight-{}",
+                nameStudent, age, course, gender, grade, height,
+                scholarship, militaryDepartment, weight);
+        LOG.warn("Student info: name-{}, age-{}, course-{}, gender-{}, "
+                        + "grade-{}, height-{}, scholarship-{}, militaryDepartment-{}, weight-{}",
+                nameStudent, age, course, gender, grade, height,
+                scholarship, militaryDepartment, weight);
+
     }
 }
