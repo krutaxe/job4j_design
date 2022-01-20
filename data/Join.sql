2ad8cc856b13a2930473df2e4edec3cfb984d0d6
Zver <krutaxe@mail.ru>
2022-01-20 07:14:13 +0300

    0. Join [#6862]

--- data/Join.sql
+++ data/Join.sql
@@ -1,9 +1,9 @@
-create table departments(
+create table departments (
     id serial primary key,
     name varchar(255)
 );
 
-create table emploers(
+create table emploers (
     id serial primary key,
     name varchar(255),
     departments_id int references departments(id)
